6e1b4c29b3ca5b6d1f0373c96bae36611b4868aa
rudrax14 <m.rudra2003@gmail.com>
2023-08-30 02:52:55 +0530

    SIGNUP page is also done for loader

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Signup = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
   const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" })
   let navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { name, email, password } = credentials
+    setIsLoading(true);
     const response = await fetch('https://onnoted-backend.onrender.com/api/auth/createuser', {
       method: 'POST',
       headers: {
@@ -15,6 +17,7 @@ const Signup = (props) => {
       },
       body: JSON.stringify({ name, email, password })
     });
+    setIsLoading(false);
     const json = await response.json()
     // console.log(json);
     if (json.success) {
@@ -33,27 +36,37 @@ const Signup = (props) => {
   return (
     <>
       <div className='mt-3'>
-        <h3>Login to continue Simple-Notez</h3>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">Name</label>
-            <input type="text" className="form-control" id="name" name='name' onChange={onChange} />
+        {!isLoading ? (
+          <div>
+            <h3>Login to continue Simple-Notez</h3>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">Name</label>
+                <input type="text" className="form-control" id="name" name='name' onChange={onChange} />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" name='email' onChange={onChange} aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password" name='password' onChange={onChange} minLength={5} required />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                <input type="password" className="form-control" id="cpassword" name='cpassword' onChange={onChange} minLength={5} required />
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
           </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" name='email' onChange={onChange} aria-describedby="emailHelp" />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        ) : (
+          <div className="position-absolute top-50 start-50 translate-middle ">
+            <div className="spinner-border text-primary fs-4" style={{ width: '3rem', height: '3rem' }} role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
           </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">Password</label>
-            <input type="password" className="form-control" id="password" name='password' onChange={onChange} minLength={5} required />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-            <input type="password" className="form-control" id="cpassword" name='cpassword' onChange={onChange} minLength={5} required />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        )}
       </div>
     </>
   )
